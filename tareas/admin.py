730514933f9a0415ce53354ed4401b7d3ed8ae45
lautaronahuelp <lautaro@outlook.com>
2024-02-14 16:08:55 -0300

    Se crean modelos Tarea, Reaccion y TareaFina

---
+++ tareas/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Tarea, TareaFina, Reaccion
+
+admin.site.register(Tarea)
+admin.site.register(TareaFina)
+admin.site.register(Reaccion)
---
+++ tareas/models.py
@@ -0,0 +1,38 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class Tarea(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    sensation = models.CharField(max_length=255, blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    deadline_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.description
+
+class Reaccion(models.Model):
+    description = models.CharField(max_length=100)
+    emoji = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.description
+
+class TareaFina(models.Model):
+    meta = models.ForeignKey(Tarea, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    order = models.SmallIntegerField()
+    satisfaction = models.ForeignKey(Reaccion, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    completed_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.description
+    
+    def complete(self):
+        self.completed_date = timezone.now()
+        self.save()
+
+
+
